16fee315545b86a7c803f5b4d11a41f6e1e3495e
Admin <Admin@DESKTOP-EQ55Q8H>
2025-07-13 10:25:49 +0530

    rename changes and application name

--- NoteApplication/src/main/java/com/NoteXApplication.java
+++ NoteApplication/src/main/java/com/NoteXApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NoteApplication {
+public class NoteXApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NoteApplication.class, args);
+		SpringApplication.run(NoteXApplication.class, args);
 	}
 
 }
